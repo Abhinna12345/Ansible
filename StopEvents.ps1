9e12cedc57ff3da6bef6c024331462a18b779e2b
anild2020 <66798399+anild2020@users.noreply.github.com>
2020-08-12 13:57:13 -0400

    Add files via upload

---
+++ StopEvents.ps1
@@ -0,0 +1,12 @@
+﻿$DataDir="c:\Scripts\Data"
+$collectors = Get-ChildItem $DataDir\*.xml
+foreach ($collector in $collectors) {
+    #Start a data collector if it is stopped
+    $query="logman query `""+$collector.BaseName+"`""
+    $logmanQuery = Invoke-Expression $query
+    #Stop existing collectors
+    if ($logmanQuery -Match "Running") {
+        Write-Output "Stoped " $collector.BaseName
+        logman stop -n $collector.BaseName
+    }
+}
